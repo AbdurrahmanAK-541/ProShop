8f2e3cd8aa247e6d3f3520b33d0401e81ec0c1d7
AbdurrahmanAK-541 <Abdurrahman@github.com>
2021-12-21 17:24:52 +0000

    This is my Header, Footer and HomeScreen so far

---
+++ frontend/build/precache-manifest.137db9f7035dbc853aad82b109b6657b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad3f8cafcf1485aa9ad8",
+    "url": "/static/css/main.4c490635.chunk.css"
+  },
+  {
+    "revision": "ad3f8cafcf1485aa9ad8",
+    "url": "/static/js/main.ad3f8caf.chunk.js"
+  },
+  {
+    "revision": "650d7f9d2c3ec1b1ce04",
+    "url": "/static/js/1.650d7f9d.chunk.js"
+  },
+  {
+    "revision": "a764e4c0ec46bcbcf5d7",
+    "url": "/static/js/2.a764e4c0.chunk.js"
+  },
+  {
+    "revision": "c70a463dd78aa135f34a",
+    "url": "/static/js/runtime~main.c70a463d.js"
+  },
+  {
+    "revision": "244b0401fb37f42e85df0e36c3a84170",
+    "url": "/index.html"
+  }
+];
