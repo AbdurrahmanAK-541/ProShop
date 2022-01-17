493623fd68046a6d44e68dbef49d64bb801201a2
AbdurrahmanAK-541 <Abdurrahman@github.com>
2022-01-17 18:44:10 +0000

    Now able to register new users successfully. tested using Postman and verified in mongoDB

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,16 @@ userSchema.methods.matchPassword = async function (inputedPassword) {
   //pass in the inputed password and compare it to the password stored
 }
 
+//adding middleware. it automatically runs, no need to import to the userController
+userSchema.pre('save', async function (next) {
+  //algorithm is executed before saving to the database
+  if (!this.isModified('password')) {
+    //the algorithm below will only be triggered if the password is modified
+    next() //if the password is not modified, it will move on
+  }
+  const salt = await bcrypt.genSalt(10) //a bcyrpt that generates a password "salt" for security reasons
+  this.password = await bcrypt.hash(this.password, salt) //plain text password is hashed and the salt is added
+})
 const User = mongoose.model('User', userSchema)
 
 export default User
